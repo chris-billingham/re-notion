0d1070f1913ba34287bc3629f8eaf5cc13b71d8b
chris-billingham <chris.billingham@gmail.com>
2023-02-18 15:40:54 +0000

    function that will get all the pages for a database, including dealing with pagination

--- api_experiments.R
+++ api_experiments.R
@@ -44,3 +44,57 @@ get_database <- function(db_id, token = Sys.getenv("NOTION_TOKEN")) {
   
   return(db_info)
 }
+
+get_database_all_pages <- function(db_id, token = Sys.getenv("NOTION_TOKEN")) {
+  # check for string
+  if(!is.character(db_id)){stop("db_id needs to be a string")}
+  # check db_id is in the right format
+  if(!str_detect(db_id, "[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}")){stop("db_id is in the wrong format. why not try using the extract_database_from_url function")}
+  
+  # build the request to query the database
+  req <- request(glue(base_url, "databases/", db_id, "/query")) %>%
+    req_method("POST") %>%
+    req_auth_bearer_token(token) %>%
+    req_headers(`Notion-Version` = '2022-06-28',
+                `Content-Type` = 'application/json')
+  
+  # perform request and turn the body into json
+  db_pages <- req %>%
+    req_perform() %>%
+    resp_body_json()
+  
+  # extract all the page ids 
+  all_pages <- db_pages %>%
+    # get the results element
+    pluck("results") %>%
+    # get all the page_ids
+    map_chr("id")
+  
+  # check if we've got any more than the max pages, if so loop whilst this is true
+  while(db_pages$has_more) {
+    # modify the request for a new start_cursor
+    req <- req %>%
+      req_body_json(list(
+        start_cursor = db_pages$next_cursor
+      ))
+    
+    # perform the request and return as json
+    db_pages <- req %>%
+      req_perform() %>%
+      resp_body_json()
+    
+    # get the new pages
+    new_pages <- db_pages %>%
+      # get the results element
+      pluck("results") %>%
+      # get all the page_ids
+      map_chr("id")
+    
+    # add to all the pages
+    all_pages <- c(all_pages, new_pages)
+  }
+  
+  # return all the pages
+  return(all_pages)
+}
+
